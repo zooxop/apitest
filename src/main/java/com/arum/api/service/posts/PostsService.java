4775e2de4e18d40c789d637a1ec0c497bf19a773
문철현 <muncheolhyeon@muncheolhyeon-ui-MacBookAir.local>
2022-01-06 00:53:19 +0900

    add function to update, delete of boardpage

--- src/main/java/com/arum/api/service/posts/PostsService.java
+++ src/main/java/com/arum/api/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.arum.api.service.posts;
 
 import com.arum.api.domain.posts.Posts;
 import com.arum.api.domain.posts.PostsRepository;
+import com.arum.api.web.dto.PostsListResponseDto;
 import com.arum.api.web.dto.PostsResponseDto;
 import com.arum.api.web.dto.PostsSaveRequestDto;
 import com.arum.api.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/arum/api/web/IndexController.java
+++ src/main/java/com/arum/api/web/IndexController.java
@@ -1,17 +1,36 @@
 package com.arum.api.web;
 
+import com.arum.api.domain.posts.PostsRepository;
+import com.arum.api.service.posts.PostsService;
+import com.arum.api.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
